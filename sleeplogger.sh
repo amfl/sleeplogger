4f64fdc9a7b40ab50c71a4181533134881fe790a
amfl <amfl@amorphousflux.net>
2014-03-06 00:16:29 +1300

    Included init.d script (Hard-coded path for now)

--- sleeplogger.sh
+++ sleeplogger.sh
@@ -1,1 +1,30 @@
-python /home/pi/sleeplogger/logger.py /home/pi/sleeplogger/sleep.log > /dev/null
+#! /bin/bash
+# /etc/init.d/sleeplogger.sh
+
+### BEGIN INIT INFO
+# Provides:          sleeplogger
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: A sleep logger in python.
+# Description:       A sleep logger in python
+### END INIT INFO
+
+# Followed this guide. http://www.stuffaboutcode.com/2012/06/raspberry-pi-run-program-at-start-up.html
+
+# Carry out specific functions when asked to by the system
+case "$1" in
+  start)
+    echo "Starting sleeplogger"
+    /home/pi/sleeplogger/sleeplogger.py /home/pi/sleeplogger/sleep.log > /dev/null &
+    ;;
+  stop)
+    echo "Stopping sleeplogger"
+    killall -9 sleeplogger.py
+    ;;
+  *)
+    echo "Usage: /etc/init.d/sleeplogger {start|stop}"
+    exit 1
+    ;;
+esac
